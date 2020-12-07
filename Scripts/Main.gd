d54b136f2f48cf1d672b29ba269e80d6c758f677
greenwoodmultimedia <greenwoodmultimedia@users.noreply.github.com>
2020-12-07 10:55:15 -0500

    Ajout des niveaux du jeu
    Création des niveaux du jeu. Tous les niveaux ont le même design de base.

--- Scripts/Main.gd
+++ Scripts/Main.gd
@@ -1,34 +1,47 @@
 extends Node
 
+onready var boutonJouerImage = $boutonJouer/boutonJouerImage;
+onready var boutonAideImage = $boutonAide/boutonAideImage;
+onready var boutonCreditsImage = $boutonCredits/boutonCreditsImage;
+onready var boutonQuitterImage = $boutonQuitter/boutonQuitterImage;
+
 func _ready():
 	pass
-
-#func _on_boutonAide_mouse_entered():
-#	if Input.is_action_just_pressed("ui_click_left"):
-#		pass
-
-#func _on_boutonCredits_mouse_entered():
-#	if Input.is_action_just_pressed("ui_click_left"):
-#		pass
-
-#func _on_boutonQuitter_mouse_entered():
-#	if Input.is_action_just_pressed("ui_click_left"):
-#		get_tree().quit();
-
 
 func _on_boutonJouer_input_event(viewport, event, shape_idx):
 	if Input.is_action_just_pressed("ui_click_left"):
 		get_tree().change_scene("res://Scenes/Niveau1.tscn");
 
-
 func _on_boutonAide_input_event(viewport, event, shape_idx):
 	pass # Replace with function body.
-
 
 func _on_boutonCredits_input_event(viewport, event, shape_idx):
 	pass # Replace with function body.
 
-
 func _on_boutonQuitter_input_event(viewport, event, shape_idx):
 	if Input.is_action_just_pressed("ui_click_left"):
 		get_tree().quit();
+
+func _on_boutonJouer_mouse_entered():
+	boutonJouerImage.modulate = Color(1,1,1,0.7);
+
+func _on_boutonJouer_mouse_exited():
+	boutonJouerImage.modulate = Color(1,1,1,1);
+
+func _on_boutonAide_mouse_entered():
+	boutonAideImage.modulate = Color(1,1,1,0.7);
+
+func _on_boutonAide_mouse_exited():
+	boutonAideImage.modulate = Color(1,1,1,1);
+
+func _on_boutonCredits_mouse_entered():
+	boutonCreditsImage.modulate = Color(1,1,1,0.7);
+
+func _on_boutonCredits_mouse_exited():
+	boutonCreditsImage.modulate = Color(1,1,1,1);
+
+func _on_boutonQuitter_mouse_entered():
+	boutonQuitterImage.modulate = Color(1,1,1,0.7);
+
+func _on_boutonQuitter_mouse_exited():
+	boutonQuitterImage.modulate = Color(1,1,1,1);
