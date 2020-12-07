5212d323b6a94ec0aed6cb5603d814cab479ec6e
greenwoodmultimedia <greenwoodmultimedia@users.noreply.github.com>
2020-12-07 11:44:21 -0500

    Ajout des limites pour le Laser du Joueur
    Les munitions du Joueur s'autodétruise quand elle entre en contact avec les limites du jeu ou quand la munition sort de l'écran.

--- Scripts/Laser.gd
+++ Scripts/Laser.gd
@@ -8,3 +8,9 @@ func _ready():
 func _process(delta):
 	var velocity = Vector2(vitesse, 0).rotated(rotation);
 	var collision = move_and_collide(velocity);
+	if collision:
+		if collision.collider.is_in_group("limiteJeu"):
+			queue_free();
+
+func _on_LaserVisible_screen_exited():
+	queue_free();
